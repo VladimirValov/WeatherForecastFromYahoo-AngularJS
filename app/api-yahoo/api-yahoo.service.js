3eb35333ec3334666030327adb220a6cb72f1b4d
Vladimir Valov <valov-web@yandex.ru>
2017-05-08 17:46:53 +0300

    Get Weather from Api Yahoo

--- app/api-yahoo/api-yahoo.service.js
+++ app/api-yahoo/api-yahoo.service.js
@@ -2,9 +2,23 @@ angular.
   module('api-yahoo').
     factory('YahooWeather', ['$resource',
       function($resource) {
-        return $resource('API-QUERY.json', {}, {
+        let urlApi = 'https://query.yahooapis.com/v1/public/yql?q=' +
+          'select * from weather.forecast where woeid = ' + ':woeid' +
+            '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+        return $resource(urlApi, {woeid : '2028717' }, {
           query: {
-            method: 'GET'            
+            method: 'GET',
+            params: {woeid: '2028717'},
+            isArray: true
+          }
+          },
+          get: {
+            method: 'GET'
+            //,
+            //headers: {
+            //  "client_id": "dj0yJmk9d1FEQ1lBUkJFaHhtJmQ9WVdrOVpFMVFWVWR3TlRBbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lZg--",
+            //  "client_secret": "eb713f09384f66814dc7a450eb6dc6d3692d7a1d"
+             //}
           }
         });
       }
