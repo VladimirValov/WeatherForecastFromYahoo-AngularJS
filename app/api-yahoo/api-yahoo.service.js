f61d70a8c4304b0b3e3115703407bd4cb8ae24d1
Vladimir Valov <valov-web@yandex.ru>
2017-05-10 05:32:28 +0300

    Переделал шаблон + рекурсивное получение данных

--- app/api-yahoo/api-yahoo.service.js
+++ app/api-yahoo/api-yahoo.service.js
@@ -5,12 +5,17 @@ angular.
         let urlApi = 'https://query.yahooapis.com/v1/public/yql?q=' +
           'select * from weather.forecast where woeid = ' + ':woeid' +
             '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+
+            //Comment this for get data fromAPI  
+            urlApi = 'API-QUERY.json';
+
+
         return $resource(urlApi, {woeid : '2028717' }, {
           query: {
             method: 'GET',
             params: {woeid: '2028717'},
             isArray: true
-          }
           },
           get: {
             method: 'GET'
